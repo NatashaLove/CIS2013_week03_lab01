eb144890e68f350607d58864d5a489e6ed9e7fd0
Natasha S Love <debora@inbox.ru>
2018-01-31 20:36:19 -0600

    for statement
    beers on the wall

--- flowControl.cpp
+++ flowControl.cpp
@@ -5,24 +5,36 @@ int main() {
 
 	char contimueProgram = 'x';
 
-	do {
-		cout << "Do you want to continue (y/n)?";
+	for (int i = 0; i <99; i++) { 
+		if (i == 1) {
+			cout << 1 << "bottle of beer on the wall" << endl;
+		}
+		else if (i == 0) {
+			cout << "No more bottles of beer on the wall" << endl;
+		}
+		else {
+			cout << i << "bottles of beer on the wall" << endl;
+		}
+	}
+
+	//do {
+		//cout << "Do you want to continue (y/n)?";
 		cin >> contimueProgram;
 
-		switch (contimueProgram) {
-		case 'y':
-			cout << "Going to continue" << endl;
-			break;
-		case 'n':
-			cout << "Going to exit" << endl;
+	//	switch (contimueProgram) {
+		//case 'y':
+		//	cout << "Going to continue" << endl;
+		//	break;
+		//case 'n':
+		//	cout << "Going to exit" << endl;
 		
-			break;
+		//	break;
 
-		default:
-			contimueProgram = 'y';
+		//default:
+		//	contimueProgram = 'y';
 
-		}
-	} while (contimueProgram == 'y');
+	//	}
+	//} while (contimueProgram == 'y');
 
 	return 0;
 
